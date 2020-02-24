c63f8cbc7383a03826361b0adb90434d2ecb213d
yaoweicheng <77356399@qq.com>
2020-02-24 16:41:54 +0800

    mapper

---
+++ src/main/java/com/lzly/crm/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package com.lzly.crm.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author 嘟嘟~
+ * @version 1.0
+ * @date 2020/2/24 16:34
+ */
+@Mapper
+public class UserMapper {
+}
